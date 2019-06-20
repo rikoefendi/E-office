c7c59e4b26cd52c217ead2f5006164f7cb2fac94
rikoefendi100 <rikoefendi100@gmail.com>
2019-06-20 23:57:39 +0700

    fixing error index

--- routes/web.php
+++ routes/web.php
@@ -16,54 +16,8 @@ use Illuminate\Http\Request;
 
 use PhpMimeMailParser\Parser;
 Route::get('/', function (Request $req) {
-     $google = new GoogleApi();
-
-     $service = new Google_Service_Gmail($google->getClient());
-//
-// // Print the labels in the user's account.
-//
-      $user = 'me';
-//
-
-      // dd($results);
-              $get = $service->users_messages->get($user, '1674d29fad32a88c');
-              dd($get);
-//       $messages = [];
-//
-//       foreach ($results['messages'] as $result) {
-//
-//         $rawData = $get['raw'];
-//         $switched = str_replace(['-', '_'], ['+', '/'], $rawData);
-//
-//         $decodedMessage = base64_decode($switched);
-//         $parser = (new Parser)->setText($decodedMessage);
-//         $messages[] = $parser->getMessageBody();
-//       }
-//       // $switched = str_replace(['-', '_'], ['+', '/'], $results);
-//
-//       dd(collect($messages));
-//
-//       // $parser->setText(base64_decode($switched));
-//       $rawData = $results['raw'];
-//     $switched = str_replace(['-', '_'], ['+', '/'], $rawData);
-//
-//     $decodedMessage = base64_decode($switched);
-//     $parser = (new Parser)->setText($decodedMessage);
-//     $uri = $parser->getAttachments()[0]->getContent();
-//     header('Content-Description: File Transfer');
-//     header('Content-Type: application/octet-stream');
-//     header('Content-Disposition: attachment; filename="test.docx"');
-//     header('Expires: 0');
-//     header('Cache-Control: must-revalidate');
-//     header('Pragma: public');
-//     return $uri;
-//     return fopen($uri, 'r');
-    // if(!$decodedMessage){
-    //     $decodedMessage = FALSE;
-    // }
-    // dd($decodedMessage);
-      // return $decodedMessage;
-})->middleware('google');
+    return view('welcome');
+});
 
 Auth::routes();
 
