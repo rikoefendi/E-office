d683d50de9cd5e395fd3026f28884330a027ca39
rikoefendi100 <rikoefendi100@gmail.com>
2019-07-22 00:52:14 +0700

    fix approved & waiting revisi

--- routes/web.php
+++ routes/web.php
@@ -20,40 +20,43 @@ Route::get('/', function (Request $req) {
 });
 
 Auth::routes();
+Route::group(['middleware' => 'auth'], function() {
 
-Route::get('/users', 'UserController@index');
-Route::get('user/create', 'UserController@create');
-Route::post('user/store', 'UserController@store');
-Route::get('user/{id}/edit', 'UserController@edit');
-Route::post('user/update', 'UserController@update');
-Route::get('user/{id}/delete', 'UserController@destroy');
 
-Route::get('/home', 'HomeController@index')->name('home');
+  Route::get('/users', 'UserController@index');
+  Route::get('user/create', 'UserController@create');
+  Route::post('user/store', 'UserController@store');
+  Route::get('user/{id}/edit', 'UserController@edit');
+  Route::post('user/update', 'UserController@update');
+  Route::get('user/{id}/delete', 'UserController@destroy');
 
-Route::get('/test', function (Request $req) {
-  $google = new \App\Http\GoogleApi();
-  if($req->code){
-    $google->getAccessTokenWithAuthCode($req->code);
-    return back();
-  }
-  if($google->isAccessTokenExpired()){
-    return redirect($google->getAuthUrl());
-  }
+  Route::get('/home', 'HomeController@index')->name('home');
 
-  return view('test');
+  Route::get('/test', function (Request $req) {
+    $google = new \App\Http\GoogleApi();
+    if($req->code){
+      $google->getAccessTokenWithAuthCode($req->code);
+      return back();
+    }
+    if($google->isAccessTokenExpired()){
+      return redirect($google->getAuthUrl());
+    }
+
+    return view('test');
+  });
+
+  Route::get('/mailbox',  'MailBoxController@mailbox');
+
+  Route::get('/compose',  'ComposeController@create');
+
+  Route::post('/compose', 'ComposeController@compose');
+  Route::get('/forward/{id}', 'ComposeController@forward');
+
+  Route::get('/read/{id}',  'MailBoxController@show');
+
+  Route::get('/read/{id}/download/{index}', 'MailBoxController@download');
+  Route::get('/mailbox/{id}/to-approval', 'MailBoxController@toApproval');
+  Route::get('/approval', 'ApprovalController@index');
+  Route::get('/approval/{id}', 'ApprovalController@show');
+  Route::get('/approval/{id}/{state}', 'ApprovalController@changeStat');
 });
-
-Route::get('/mailbox',  'MailBoxController@mailbox');
-
-Route::get('/compose',  'ComposeController@create');
-
-Route::post('/compose', 'ComposeController@compose');
-Route::get('/forward/{id}', 'ComposeController@forward');
-
-Route::get('/read/{id}',  'MailBoxController@show');
-
-Route::get('/read/{id}/download/{index}', 'MailBoxController@download');
-Route::get('/mailbox/{id}/to-approval', 'MailBoxController@toApproval');
-Route::get('/approval', 'ApprovalController@index');
-Route::get('/approval/{id}', 'ApprovalController@show');
-Route::get('/approval/{id}/{state}', 'ApprovalController@changeStat');
